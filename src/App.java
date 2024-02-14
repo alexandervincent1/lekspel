79b830d6ec555079c0a6cc7d03dd2cf67a178189
alexandervincent1 <alexander.vincent@elev.ga.ntig.se>
2024-02-14 11:05:36 +0100

    lagt till massa skoj

--- src/App.java
+++ src/App.java
@@ -3,67 +3,100 @@ import java.util.Random;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        startspel();
+        spelet();
+
+    }
+
+    public static void startspel() {
         Scanner tangentbord = new Scanner(System.in);
-        System.out.println("Meny");
-        System.out.println("1. Uppercut " + "2.Hook" + " 3.Bangkok");
-        int menyval = tangentbord.nextInt();
+        System.out.println(" ");
+        System.out.println("Tryck på enter för och starta spel");
+        
+        String startaspell = tangentbord.nextLine();
+        
+
+    }
+
+    public static void spelet() {
+        Scanner tangentbord = new Scanner(System.in);
+        int spelarensliv = 100;
+        int datornsliv = 100;
+        
+        
+        while (true) {
+            int menyval = 0;
         Random tärning = new Random();
         int slumptal = tärning.nextInt(100);
         int uppercut = slumptal;
         int hook = slumptal;
         int bangkok = slumptal;
-        int spelarensliv = 100;
-        int datornsliv = 100;
         Random tärning1 = new Random();
         int slumptal1 = tärning1.nextInt(100);
-        
+        int runda = 0;
+        int vinstspelare=0;
+        System.out.println(" ");
+            System.out.println("1. Uppercut " + "2.Hook" + " 3.Bangkok");
+            menyval = tangentbord.nextInt();
 
-        while (menyval > 0) {
-            switch (menyval) {
-                case 1:
-                    datornsliv = datornsliv - uppercut;
-                    spelarensliv = spelarensliv - slumptal1;
-                    System.out.println("Uppercut gjorde " + uppercut + " skada");
-                    System.out.println("Datorn gjorde " + slumptal1 + " skada");
-                    System.out.println("Du har " + spelarensliv + " liv kvar");
-                    System.out.println("Datorn har " + datornsliv + " liv kvar");
-                    System.out.println("1. Uppercut " + "2.Hook" + " 3.Bangkok");
-                    menyval = tangentbord.nextInt();
-                    break;
-
-                case 2:
-                    datornsliv = datornsliv - hook;
-                    spelarensliv = spelarensliv - slumptal1;
-                    System.out.println("Hook gjorde " + hook + " skada");
-                    System.out.println("Datorn gjorde " + slumptal1 + " skada");
-                    System.out.println("Du har " + spelarensliv + " liv kvar");
-                    System.out.println("Datorn har " + datornsliv + " liv kvar");
-                    System.out.println("1. Uppercut " + "2.Hook" + " 3.Bangkok");
-                    menyval = tangentbord.nextInt();
-                    break;
-                case 3:
-                    datornsliv = datornsliv - bangkok;
-                    spelarensliv = spelarensliv - slumptal1;
-                    System.out.println("Bangkok gjorde " + bangkok + " skada");
-                    System.out.println("Datorn gjorde " + slumptal1 + " skada");
-                    System.out.println("Du har " + spelarensliv + " liv kvar");
-                    System.out.println("Datorn har " + datornsliv + " liv kvar");
-                    System.out.println("1. Uppercut " + "2.Hook" + " 3.Bangkok");
-                    menyval = tangentbord.nextInt();
-                    break;
+            if (menyval == 1) {
+                datornsliv = datornsliv - uppercut;
+                spelarensliv = spelarensliv - slumptal1;
+                System.out.println(" ");
+                System.out.println("Uppercut gjorde " + uppercut + " skada");
+                System.out.println("Datorn gjorde " + slumptal1 + " skada");
+                System.out.println("Du har " + spelarensliv + " liv kvar");
+                System.out.println("Datorn har " + datornsliv + " liv kvar");
+              
+            }
+            if (menyval == 2) {
+                datornsliv = datornsliv - hook;
+                spelarensliv = spelarensliv - slumptal1;
+                System.out.println(" ");
+                System.out.println("Hook gjorde " + hook + " skada");
+                System.out.println("Datorn gjorde " + slumptal1 + " skada");
+                System.out.println("Du har " + spelarensliv + " liv kvar");
+                System.out.println("Datorn har " + datornsliv + " liv kvar");
+                         }
+            if (menyval == 3) {
+                datornsliv = datornsliv - bangkok;
+                spelarensliv = spelarensliv - slumptal1;
+                System.out.println(" ");
+                System.out.println("Bangkok gjorde " + bangkok + " skada");
+                System.out.println("Datorn gjorde " + slumptal1 + " skada");
+                System.out.println("Du har " + spelarensliv + " liv kvar");
+                System.out.println("Datorn har " + datornsliv + " liv kvar");
+                
 
             }
-        }
+            if (menyval==10) {
+        
+                
+            }
+            if (datornsliv <= 0) {
+                menyval = 10;
+                System.out.println(" ");
+                System.out.println("Du vann");
+                runda++;
+                vinstspelare++;
+                System.out.println(" ");
+                System.out.println("Du har kört "+ runda+" och vunnit "+vinstspelare);
+                datornsliv =100;
+                spelarensliv = 100;
+                startspel();
+            }
+            else if (spelarensliv<=0) {
+                menyval=10;
+                System.out.println("Du torska");
+                runda++;
+                System.out.println("Du har kört "+ runda+" och vunnit "+vinstspelare);
+                spelarensliv = 100;
+                datornsliv =100;
+                startspel();
+            }
 
-    
-    }
-    static int slut (datornsliv,spelarensliv){
-        if (spelarensliv<=0) {
-            System.out.println("Du förlorarade mot Hitler");
         }
-        else if (datornslivliv<=0) {
-            System.out.println("Du vann mot Datorn");
-        }
+        
+
     }
 }
-
